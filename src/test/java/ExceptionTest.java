03b119ecf50e1db90d8e9e40881a88ff24626949
lidongbo <lidongbo@caijingquan.com>
2017-03-15 11:06:05 +0800

    cast exception generic

--- src/test/java/ExceptionTest.java
+++ src/test/java/ExceptionTest.java
@@ -6,12 +6,15 @@ import java.sql.SQLException;
  * Created by aaa on 17-3-15.
  */
 public class ExceptionTest<T extends Exception> {
+//public class ExceptionTest<T extends RuntimeException> {
     @Test
     public void run(){
         try {
             new ExceptionTest<RuntimeException>().castT(new SQLException());
         }catch (final RuntimeException/*or Exception*/ e){
+//        }catch (final Exception e){
 //        }catch (final SQLException e){
+//          Can be reached by Exception
             e.printStackTrace();
         }
     }

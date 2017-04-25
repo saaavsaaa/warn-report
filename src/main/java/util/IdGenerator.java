d9c9c068b5665418a66c4a19794fd4b827bcef6d
lidongbo <lidongbo@caijingquan.com>
2017-04-25 18:36:45 +0800

    global unique id

---
+++ src/main/java/util/IdGenerator.java
@@ -0,0 +1,69 @@
+package util;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Created by root on 17-4-25.
+ */
+public enum  IdGenerator {
+    INSTANCE;
+    
+    private String host;
+    IdGenerator(){
+        host = getHost();
+    }
+    
+    public synchronized String createNewId(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(host);
+        stringBuilder.append("-");
+        stringBuilder.append(getIncr());
+        stringBuilder.append("-");
+        stringBuilder.append(getTick());
+        stringBuilder.append("-");
+        stringBuilder.append(getThreadId());
+        return stringBuilder.toString();
+    }
+    
+    private static LongAdder incr = new LongAdder();
+    private static long getIncr(){
+        incr.increment();
+        return incr.longValue();
+    }
+    
+    private static long getThreadId(){
+        return Thread.currentThread().getId();
+    }
+    
+    private static long getTick(){
+        return System.currentTimeMillis();
+    }
+    
+    private String getHost() {
+        Enumeration<NetworkInterface> ni = null;
+        try {
+            ni = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface netCard = ni.nextElement();
+            byte[] bytes = netCard.getHardwareAddress();
+            if(netCard != null && bytes != null && bytes.length == 6){
+                StringBuffer sb = new StringBuffer().append(bytes[0]).append(bytes[1]).append(bytes[2]).append(bytes[3])
+                        .append(bytes[4]).append(bytes[5]);
+                return sb.toString().toUpperCase();
+            }
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        }
+        return "12700001";
+    }
+    
+    private static String toChar(byte b){
+        //与11110000作按位与运算以便读取当前字节高4位
+        String high = Integer.toHexString((b&240)>>4);
+        //与00001111作按位与运算以便读取当前字节低4位
+        String low = Integer.toHexString(b&15);
+        return high + low;
+    }
+}

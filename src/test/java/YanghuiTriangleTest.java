88616193c83842731ce945b6b616a068246aae59
lidongbo <lidongbo@caijingquan.com>
2017-06-26 16:17:25 +0800

    adjust interval

--- src/test/java/YanghuiTriangleTest.java
+++ src/test/java/YanghuiTriangleTest.java
@@ -7,33 +7,31 @@ import java.util.*;
  * Created by aaa on 17-6-26.
  */
 public class YanghuiTriangleTest {
-    private static final String Interval_In_Int = "    ";
-    private static final String Interval_In_Layer_Int = "  ";
-    private static final String Interval_Exsit_Int = " ";
+    private static final char Interval = ' ';
     final static int [] sizeTable = { 9, 99, 999, 9999, 99999, 999999, 9999999,
             99999999, 999999999, Integer.MAX_VALUE };
 
     @Test
     public void build(){
-        int layerRowsExceptTop = 16;
+        int layerRowsExceptTop = 30;
         ArrayListExtend<Integer> upLayer = new ArrayListExtend<>();
         upLayer.addInThis(0).addInThis(1).addInThis(0);
-        System.out.println(StringUtils.leftPad("1", layerRowsExceptTop, Interval_In_Layer_Int));
+        System.out.println(StringUtils.leftPad("1", layerRowsExceptTop * 2, Interval));
         int beginLayer = 2;
         int upAddA = 0;
         int upAddB = 0;
         for (int r = layerRowsExceptTop; r > 0; r--, beginLayer++) {
             ArrayListExtend<Integer> newLayer = new ArrayListExtend<>();
             newLayer.addInThis(0);
-            String print = StringUtils.leftPad("", r - 1, Interval_Exsit_Int);
+            String print = StringUtils.leftPad("", (r - 1) * 4, Interval);
             for(int i = 0; i < beginLayer; i++) {
                 upAddA = upLayer.get(i) > -1 ? upLayer.get(i) : 0;
                 upAddB = upLayer.get(i + 1) > -1 ? upLayer.get(i + 1) : 0;
                 int e = upAddA + upAddB;
                 newLayer.addInThis(e);
-                String value = StringUtils.leftPad(String.valueOf(e), getDigit(layerRowsExceptTop), Interval_Exsit_Int);
+                String value = StringUtils.leftPad(String.valueOf(e), getDigit(layerRowsExceptTop), Interval);
                 print += value;
-                print += Interval_In_Int;
+                print += "       ";
             }
             upLayer = newLayer.addInThis(0);
             System.out.println(print);

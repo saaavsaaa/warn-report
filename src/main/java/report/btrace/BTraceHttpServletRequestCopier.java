fa4f1c89f87c6a27e24c740ce6ffbd6d95c60595
lidongbo <lidongbo@caijingquan.com>
2017-09-27 11:16:39 +0800

    btrace

---
+++ src/main/java/report/btrace/BTraceHttpServletRequestCopier.java
@@ -0,0 +1,101 @@
+package report.btrace;
+
+import com.sun.btrace.annotations.*;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static com.sun.btrace.BTraceUtils.*;
+
+/**
+ * Created by aaa on 17-9-8.
+ */
+@BTrace
+public class BTraceHttpServletRequestCopier {
+    @TLS
+    private static long startTime = 0;
+    private static long initStartTime = 0;
+    
+    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge")
+    public static void startMethod(){
+        startTime = timeMillis();
+    }
+    
+    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge", location = @Location(Kind.RETURN))
+    public static void endHttpServletRequestCopierMethod(){
+        if (timeMillis()-startTime > 1000) {
+            println(strcat("copyLarge execute time==", str(timeMillis() - startTime)));
+            println(strcat("copyLarge time==", str(Time.timestamp())));
+            println("-------------------------------------------");
+        }
+    }
+    
+    @OnLowMemory(pool = "nio", threshold = 75)
+    public static void low(){
+        println(strcat("OnLowMemory time==", str(Time.timestamp())));
+    }
+//    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge", location = @Location(Kind.RETURN))
+//    public static void traceExecute(@Self Object  copier){
+//        Object reader = get(field("cn.caijingquan.p2p.app.filter.HttpServletRequestCopier","reader"), copier);
+//        println(strcat("the class name=>", str(reader)));
+//    }
+    
+    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge", location = @Location(Kind.RETURN))
+    public static void traceHttpServletRequestCopierExecute(@ProbeClassName String name,@ProbeMethodName String method, @Return long result){
+        if (timeMillis()-startTime > 1000) {
+            println(strcat("traceHttpServletRequestCopierExecute the class name=>", name));
+            println(strcat("traceHttpServletRequestCopierExecute the class method=>", method));
+            println(strcat("traceHttpServletRequestCopierExecute result class method=>", str(result)));
+        }
+    }
+    
+    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge", location = @Location(Kind.RETURN))
+    public static void traceHttpServletRequestCopierExecuteInput(@ProbeClassName String name, @ProbeMethodName String method,
+                                                                 InputStream input, OutputStream output, byte[] buffer){
+        if (timeMillis()-startTime > 1000) {
+            println(strcat("traceHttpServletRequestCopierExecuteInput the class name=>", name));
+            println(strcat("traceHttpServletRequestCopierExecuteInput the class method=>", method));
+            println(strcat("traceHttpServletRequestCopierExecuteInput class input=>", str(input)));
+            println(strcat("traceHttpServletRequestCopierExecuteInput class output=>", str(output)));
+            println(strcat("traceHttpServletRequestCopierExecuteInput class buffer=>", str(buffer)));
+        }
+    }
+    
+    @OnMethod(clazz = "org.apache.commons.io.IOUtils", method = "copyLarge", location = @Location(Kind.ERROR))
+    public static void traceERRORExecute(@ProbeClassName String name,@ProbeMethodName String method){
+        if (timeMillis()-startTime > 1000) {
+            println(strcat("ERROR class name=>", name));
+            println(strcat("ERROR class method=>", method));
+        }
+    }
+    
+    @OnMethod(clazz = "cn.xxx.xxx.app.filter.HttpServletRequestCopier", method = "<init>")
+    public static void startMethodinit(){
+        initStartTime = timeMillis();
+    }
+    
+    @OnMethod(clazz = "cn.xxx.xxx.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
+    public static void endHttpServletRequestCopierMethodinit(){
+        if (timeMillis()-initStartTime > 1000) {
+            println(strcat("HttpServletRequestCopier.init execute time==", str(timeMillis() - initStartTime)));
+            println("-------------------------------------------");
+        }
+    }
+    
+    @OnMethod(clazz = "cn.xxx.xxx.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
+    public static void traceHttpServletRequestCopierExecuteinit(@ProbeClassName String name,@ProbeMethodName String method){
+        if (timeMillis()-initStartTime > 1000) {
+            println(strcat("the class name=>", name));
+            println(strcat("the class method=>", method));
+        }
+    }
+    
+    @OnMethod(clazz = "cn.xxx.xxx.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.ERROR))
+    public static void traceERRORExecuteinit(@ProbeClassName String name,@ProbeMethodName String method){
+        if (timeMillis()-initStartTime > 1000) {
+            println(strcat("ERROR class name=>", name));
+            println(strcat("ERROR class method=>", method));
+        }
+    }
+}
---
+++ src/main/java/report/btrace/BTraceStarter.java
@@ -0,0 +1,31 @@
+package report.btrace;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+/**
+ * Created by aaa on 17-9-27.
+ */
+public class BTraceStarter {
+    public static void main(String[] args) throws Exception {
+        
+        String classpath = System.getProperty("java.class.path");
+        if (!classpath.contains("tools.jar")) {
+            throw new RuntimeException("请在类路径中设置tools.jar!");
+        }
+        
+        System.setProperty("com.sun.btrace.probeDescPath", ".");
+        System.setProperty("com.sun.btrace.dumpClasses", "true");
+        System.setProperty("com.sun.btrace.dumpDir", "./dump");
+        System.setProperty("com.sun.btrace.debug", "false");
+        System.setProperty("com.sun.btrace.unsafe", "true");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("请输入目标进程PID： ");
+        String pid = in.readLine();
+        
+        String script = new File(BTraceStarter.class.getResource("BTraceHttpServletRequestCopier.class").getFile()).getCanonicalPath();
+        com.sun.btrace.client.Main.main(new String[] { pid, script });
+    }
+}

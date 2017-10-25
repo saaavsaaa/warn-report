089ff2f5fb1057d81b4461a1cef6014ed4b5ad0e
lidongbo <lidongbo@caijingquan.com>
2017-10-25 16:35:42 +0800

    btrace

--- src/main/java/run/CurrentTrace.java
+++ src/main/java/run/CurrentTrace.java
@@ -13,8 +13,6 @@ public enum CurrentTrace {
     INSTANCE;
     
     public static void main(String[] args) throws Exception {
-        CurrentTrace.INSTANCE.startWatchTrace();
-        Thread.sleep(3000);
         CurrentTrace.INSTANCE.started("aaaaaaa");
         System.out.println(CurrentTrace.INSTANCE.getTraceIdHolder().size());
         Thread.sleep(3000);
@@ -26,8 +24,9 @@ public enum CurrentTrace {
     }
     
     private final Integer start = 0;
-    private final Integer success = 2;
     private final Integer fail = 1;
+    private final Integer success = 2;
+    private final Integer stop = 3;
     
     private final Map<String, Integer> traceIdHolder = new ConcurrentHashMap<>();
     
@@ -38,6 +37,7 @@ public enum CurrentTrace {
     public synchronized boolean started(String traceId) {
         if (!traceIdHolder.containsKey(traceId)) {
             traceIdHolder.put(traceId, start);
+            System.out.println("start:" + traceIdHolder.size());
             return false;
         }
         return traceIdHolder.get(traceId) == fail;
@@ -52,24 +52,27 @@ public enum CurrentTrace {
     }
     
     private synchronized void stop(){
-        System.out.println("count:" + traceIdHolder.size());
+        System.out.println(System.currentTimeMillis() + " stop count:" + traceIdHolder.size());
         if (traceIdHolder.isEmpty()){
             return;
         }
         traceIdHolder.forEach(
                 (k, v)->{
                     if (v == success){
+                        traceIdHolder.replace(k, stop);
+                        System.out.println(System.currentTimeMillis() + " stop:" + k);
+                    } else if (v == stop){
                         traceIdHolder.remove(k);
-                        System.out.println("del:" + k);
+                        System.out.println(System.currentTimeMillis() + " del:" + k);
                     }
                 }
         );
     }
     
-    public void startWatchTrace() {
-        ScheduledExecutorService scheduledExecutorService = Executors
-                .newSingleThreadScheduledExecutor(r -> new Thread(r, "Current-Trace-Stop"));
+    private void startWatchTrace() {
+        ScheduledExecutorService scheduledExecutorService =
+                Executors.newSingleThreadScheduledExecutor(r -> new Thread(r, "Current-Trace-Stop"));
         
-        scheduledExecutorService.scheduleAtFixedRate(() -> stop(), 3000, 1000, TimeUnit.MILLISECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(() -> stop(), 1000, 6000, TimeUnit.MILLISECONDS);
     }
 }

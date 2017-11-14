eae1cbe57989319e2ef7c9b59b2296784e1db05c
lidongbo <lidongbo@caijingquan.com>
2017-11-14 14:30:26 +0800

    btrace

--- src/main/java/report/btrace/TraceAppRSA.java
+++ src/main/java/report/btrace/TraceAppRSA.java
@@ -16,39 +16,39 @@ public class TraceAppRSA {
     private static long initStartTime = 0;
     private static long contrast = 100;
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.util.cipher.AppRSA", method = "decryptBASE64")
+    @OnMethod(clazz = "com.***.common.util.cipher.AppRSA", method = "decryptBASE64")
     public static void startMethod(){
         startTime = timeMillis();
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "com.***.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.RETURN))
     public static void endHttpServletRequestCopierMethod(){
-        println(strcat("com.caijinquan.p2p.common.util.cipher.AppRSA.decodeBase64 execute time============", str(timeMillis()-startTime)));
+        println(strcat("com.***.common.util.cipher.AppRSA.decodeBase64 execute time============", str(timeMillis()-startTime)));
         println("-------------------------------------------");
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "com.***.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.RETURN))
     public static void traceHttpServletRequestCopierExecute(@ProbeClassName String name,@ProbeMethodName String method, String key){
         println(strcat("trace class name ============", name));
         println(strcat("trace class method ============", method));
         println(strcat("trace class base64Data ============", key));
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.ERROR))
+    @OnMethod(clazz = "com.***.common.util.cipher.AppRSA", method = "decryptBASE64", location = @Location(Kind.ERROR))
     public static void traceERRORExecute(@ProbeClassName String name,@ProbeMethodName String method){
         println(strcat("ERROR class name ============", name));
         println(strcat("ERROR class method ============", method));
     }
     
     
-    //com.caijinquan.p2p.app.controller.filter.httpobject
-    //cn.caijingquan.p2p.app.filter
-    @OnMethod(clazz = "cn.caijingquan.p2p.app.filter.HttpServletRequestCopier", method = "<init>")
+    //com.***.app.controller.filter.httpobject
+    //cn.***.app.filter
+    @OnMethod(clazz = "cn.***.app.filter.HttpServletRequestCopier", method = "<init>")
     public static void startMethodinit(){
         initStartTime = timeMillis();
     }
     
-    @OnMethod(clazz = "cn.caijingquan.p2p.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "cn.***.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
     public static void endHttpServletRequestCopierMethodinit(){
         if (timeMillis()-initStartTime > contrast) {
             println(strcat("HttpServletRequestCopier.init execute time==", str(timeMillis() - initStartTime)));
@@ -56,7 +56,7 @@ public class TraceAppRSA {
         }
     }
     
-    @OnMethod(clazz = "cn.caijingquan.p2p.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "cn.***.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.RETURN))
     public static void traceHttpServletRequestCopierExecuteinit(@ProbeClassName String name,@ProbeMethodName String method){
         if (timeMillis()-initStartTime > contrast) {
             println(strcat("the class name=>", name));
@@ -64,7 +64,7 @@ public class TraceAppRSA {
         }
     }
     
-    @OnMethod(clazz = "cn.caijingquan.p2p.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.ERROR))
+    @OnMethod(clazz = "cn.***.app.filter.HttpServletRequestCopier", method = "<init>", location = @Location(Kind.ERROR))
     public static void traceERRORExecuteinit(@ProbeClassName String name,@ProbeMethodName String method){
         if (timeMillis()-initStartTime > contrast) {
             println(strcat("ERROR class name=>", name));
--- src/main/java/report/btrace/UtilBase64Trace.java
+++ src/main/java/report/btrace/UtilBase64Trace.java
@@ -14,25 +14,25 @@ public class UtilBase64Trace {
     @TLS
     private static long startTime = 0;
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.bos.reapel.client.utils.Base64", method = "decodeBase64")
+    @OnMethod(clazz = "com.***.common.bos.reapel.client.utils.Base64", method = "decodeBase64")
     public static void startMethod(){
         startTime = timeMillis();
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "com.***.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.RETURN))
     public static void endMethod(){
-        println(strcat("com.caijinquan.p2p.common.bos.reapel.client.utils.decodeBase64 execute time============", str(timeMillis()-startTime)));
+        println(strcat("com.***.common.bos.reapel.client.utils.decodeBase64 execute time============", str(timeMillis()-startTime)));
         println("-------------------------------------------");
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.RETURN))
+    @OnMethod(clazz = "com.***.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.RETURN))
     public static void traceExecute(@ProbeClassName String name,@ProbeMethodName String method, byte[] base64Data){
         println(strcat("trace class name ============", name));
         println(strcat("trace class method ============", method));
         println(strcat("trace class base64Data ============", Strings.str(base64Data)));
     }
     
-    @OnMethod(clazz = "com.caijinquan.p2p.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.ERROR))
+    @OnMethod(clazz = "com.***.common.bos.reapel.client.utils.Base64", method = "decodeBase64", location = @Location(Kind.ERROR))
     public static void traceERRORExecute(@ProbeClassName String name,@ProbeMethodName String method){
         println(strcat("ERROR class name ============", name));
         println(strcat("ERROR class method ============", method));

2b6b9a6e8f022d86efdb18900ae1d7a16d364224
saaav <draculav@hotmail.com>
2018-01-15 15:59:12 +0800

    class visitor

--- src/main/java/code/visit/VisitClassLoader.java
+++ src/main/java/code/visit/VisitClassLoader.java
@@ -1,5 +1,7 @@
 package code.visit;
 
+import jdk.internal.org.objectweb.asm.ClassWriter;
+
 /**
  * Created by aaa on 18-1-4.
  */
@@ -21,5 +23,21 @@ public class VisitClassLoader extends ClassLoader {
     * */
     public Class<?> defineClass(String name, byte[] b){
         return super.defineClass(name, b, 0, b.length);
+    }
+    
+    @Override
+    protected Class findClass(String name)
+            throws ClassNotFoundException {
+        if (name.endsWith("_Stub")) {
+            ClassWriter cw = new ClassWriter(0);
+            
+            /*
+            在运行过程中生成所请求的类
+            */
+            
+            byte[] b = cw.toByteArray();
+            return defineClass(name, b, 0, b.length);
+        }
+        return super.findClass(name);
     }
 }

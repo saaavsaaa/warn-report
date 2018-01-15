6f6e09b37e23e545d292120695f38f8458d6b6f3
saaav <draculav@hotmail.com>
2018-01-15 12:19:50 +0800

    class visitor

--- src/main/java/code/visit/ClassReaderTest.java
+++ src/main/java/code/visit/ClassReaderTest.java
@@ -12,7 +12,7 @@ import static jdk.internal.org.objectweb.asm.Opcodes.ASM4;
 public class ClassReaderTest {
     public static void main(String[] args) throws IOException {
         ClassPrinter cp = new ClassPrinter();
-        ClassReader cr = new ClassReader("code.record.WaitClearCode");
+        ClassReader cr = new ClassReader("code.record.ClearedCode");
         cr.accept(cp, 0);
     }
 }
@@ -22,35 +22,41 @@ class ClassPrinter extends ClassVisitor {
         super(ASM4);
     }
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        System.out.println(name + " extends " + superName + " {");
+        System.out.println("signature :" + signature + ", version : " + version + ", interfaces: " + interfaces);
+        System.out.println("visit =====name :" + name + " extends superName:" + superName + " {");
     }
     
     public void visitSource(String source, String debug) {
+        System.out.println("visitSource =====================source : " + source + ", debug" + debug);
     }
     
     public void visitOuterClass(String owner, String name, String desc){
+        System.out.println("visitOuterClass =====================owner : " + owner + ", name" + name + ", desc" + desc);
     }
     
     public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+        System.out.println("visitAnnotation ===============================desc:" + desc);
         return null;
     }
     
     public void visitAttribute(Attribute attr) {
+        System.out.println("visitAttribute ======================" + attr.type);
     }
     
     public void visitInnerClass(String name, String outerName, String innerName, int access) {
     }
+    
     public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-        System.out.println(" " + desc + " " + name);
+        System.out.println("visitField ===============================desc:" + desc + ", name :" + name + ", signature :" + signature + ", value :" + value);
         return null;
     }
     
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-        System.out.println(" " + name + desc);
+        System.out.println("visitMethod ===============================desc:" + desc + ", name :" + name + ", signature :" + signature);
         return null;
     }
     
     public void visitEnd() {
-        System.out.println("}");
+        System.out.println("visitEnd ===============================}");
     }
 }

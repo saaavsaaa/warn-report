0d29473c0782cc2c0d63f5c34306aa7cb18be0ad
saaav <draculav@hotmail.com>
2018-01-31 11:02:34 +0800

    visitor

--- src/main/java/code/visit/ClassVisitorTest.java
+++ src/main/java/code/visit/ClassVisitorTest.java
@@ -98,3 +98,19 @@ class ChangeVersionAdapter extends ClassVisitor {
 */
     
 }
+
+class MultiClassAdapter extends ClassVisitor {
+    protected ClassVisitor[] cvs;
+    
+    public MultiClassAdapter(ClassVisitor[] cvs) {
+        super(ASM4);
+        this.cvs = cvs;
+    }
+    
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        for (ClassVisitor cv : cvs) {
+            cv.visit(version, access, name, signature, superName, interfaces);
+        }
+    }
+}

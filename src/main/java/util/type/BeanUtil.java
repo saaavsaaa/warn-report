fc091ade51321be24a59a71323c1022768ead39f
saaav <draculav@hotmail.com>
2018-02-02 11:29:42 +0800

    btrace

--- src/main/java/util/type/BeanUtil.java
+++ src/main/java/util/type/BeanUtil.java
@@ -1,11 +1,37 @@
 package util.type;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by aaa on 17-6-26.
  */
 public class BeanUtil {
+    public static Map<Integer, String> get(Object instance) {
+        Class clazz = instance.getClass();
+        try {
+            Field[] valueFields = clazz.getDeclaredFields();
+            Map<Integer, String> pairs = new HashMap();
+            for (Field one : valueFields) {
+                one.setAccessible(true);
+                Integer value;
+                try {
+                    value = one.getInt(instance);
+                }catch (Exception ee){
+                    value = Integer.valueOf(String.valueOf(one.get(instance)));
+                }
+                
+                String key = one.getName();
+                pairs.put(value, key);
+            }
+            return pairs;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
     public static Object get(Object instance, String key) {
         Class clazz = instance.getClass();
         try {

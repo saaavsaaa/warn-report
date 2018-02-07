2ec04db5942a2345be7b587b20bee480fe3261b0
saaav <draculav@hotmail.com>
2018-02-07 12:05:27 +0800

    visitor

--- src/main/java/code/visit/ClassMethodVisitor.java
+++ src/main/java/code/visit/ClassMethodVisitor.java
@@ -21,12 +21,11 @@ public class ClassMethodVisitor {
         MethodVisitor get = cv.visitMethod(ACC_PUBLIC, "getTTT", "()Ljava/lang/String;", null, null); // ClassWriterTest
     
         get.visitCode();
-        get.visitVarInsn(ALOAD, 0);     //读取局部变量 0(它在为这个方法调用创建帧期间被初始化为 this),并将这个值压入操作数栈中
-    
-        //从栈中弹出这个值,即 this,并将这个对象的 ttt 字段压入栈中, 即 this.ttt
+        get.visitVarInsn(ALOAD, 0); //读取局部变量 0(它在为这个方法调用创建帧期间被初始化为 this),并将这个值压入操作数栈中
+        //从栈中弹出这个值,即 this,并将这个对象的 ttt 字段(this.ttt)压入栈中
         get.visitFieldInsn(GETFIELD, "code/record/WaitClearCode", "ttt", "I");
         get.visitInsn(ARETURN);  //从栈中弹出这个值,并将其返回给调用者
-        get.visitMaxs(1, 1);
+        get.visitMaxs(1, 1); //一个局部变量和一个操作数栈空间,ClassWriter使用0初始化，所以参数有效
         get.visitEnd();
         
         MethodVisitor set = cv.visitMethod(ACC_PUBLIC, "setTTT", "(Ljava/lang/String;)V", null, null);
@@ -36,21 +35,21 @@ public class ClassMethodVisitor {
         set.visitJumpInsn(IFNULL, label); //从栈中弹出值,判断是否为null，如果是null,则跳转到由 label 标记指定的指令,否则不做任何事情
         set.visitVarInsn(ALOAD, 0); //将 this 压入操作数栈
         set.visitVarInsn(ALOAD, 1); //压入局部变量 1,在为这个方法调用创建帧期间,以 ttt 参数初始化该变量
-        //弹出这两个值,并将 int 值存储在被引用对象的 ttt 字段中,即存储在 this.ttt 中
+        //弹出这两个值,并将 int 值存储在被引用对象的 ttt 字段(this.ttt)中
         set.visitFieldInsn(PUTFIELD, "code/record/WaitClearCode", "AAA", "Ljava/lang/String;");
         Label end = new Label();
-        set.visitJumpInsn(GOTO, end); //无条件跳转到由end 标记指定的指令,也就是 RETURN 指令
+        set.visitJumpInsn(GOTO, end); //无条件跳转到由end label 指定的指令,也就是 RETURN 指令
         set.visitLabel(label);
         set.visitFrame(F_SAME, 0, null, 0, null);
         set.visitTypeInsn(NEW, "java/lang/IllegalArgumentException"); //创建一个异常对象,并将它压入操作数栈中
         set.visitInsn(DUP); //DUP 指令在栈中创建一个该对象引用的副本并入栈，为了在INVOKESPECIAL指令将对象引用出栈后还可以使用这个对象的引用
         //INVOKESPECIAL 指令弹出这两个引用之一,并对其调用异常构造器
         set.visitMethodInsn(INVOKESPECIAL, "java/lang/IllegalArgumentException", "<init>", "()V", false);
-        set.visitInsn(ATHROW); //ATHROW 指令弹出剩下的副本,并将它作为异常抛出
+        set.visitInsn(ATHROW); //ATHROW 指令弹出剩下的引用,并将它作为异常抛出
         set.visitLabel(end);
         set.visitFrame(F_SAME, 0, null, 0, null);
         set.visitInsn(RETURN);
-        set.visitMaxs(2, 2);
+        set.visitMaxs(2, 2); //两个局部变量和两个操作数栈空间
         set.visitEnd();
         cv.visitEnd();
     

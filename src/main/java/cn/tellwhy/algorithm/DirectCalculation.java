3993e804851dcd212ae067f50a811c5da2357442
saaavsaaa <draculav@hotmail.com>
2018-10-11 17:43:07 +0800

    direct calculate

---
+++ src/main/java/cn/tellwhy/algorithm/DirectCalculation.java
@@ -0,0 +1,35 @@
+package cn.tellwhy.algorithm;
+
+/*
+* 简单的计算
+*/
+public class DirectCalculation {
+
+    //方差s^2 = [(x_1 - x_avg)^2 +...(x_n - x_avg)^2] / n
+    public static double doVariance(final double[] inputs) {
+        return calculateSquareReduce(inputs) / inputs.length;
+    }
+
+    //标准差σ=sqrt(s^2)
+    public static double StandardDeviation(final double[] inputs) {
+        return Math.sqrt(doVariance(inputs));
+    }
+
+    private static double calculateAverage(final double[] inputs){
+        double accumulate = 0;
+        for (double each : inputs) {//求和
+            accumulate += each;
+        }
+        return accumulate / inputs.length;//求平均值
+    }
+
+    private static double calculateSquareReduce(final double[] inputs){
+        double avg = calculateAverage(inputs);
+        double squares = 0;
+
+        for (double each : inputs) {//求方差
+            squares += (each - avg) * (each - avg);
+        }
+        return squares;
+    }
+}

46251ee96c1289f3d8b06d1a0627e0def0f9c6ce
saaavsaaa <draculav@hotmail.com>
2018-11-27 10:02:48 +0800

    log

---
+++ src/main/java/cn/tellwhy/algorithm/BaseCalculation.java
@@ -0,0 +1,7 @@
+package cn.tellwhy.algorithm;
+
+public class BaseCalculation {
+    public static double log(final double value, final double base) {
+        return Math.log(value) / Math.log(base);
+    }
+}

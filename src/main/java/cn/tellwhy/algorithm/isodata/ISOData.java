5366f81ae4432bcc6fae6a2ae77c4c5934ddac24
saaavsaaa <draculav@hotmail.com>
2019-04-19 17:55:37 +0800

    isodata

--- src/main/java/cn/tellwhy/algorithm/isodata/ISOData.java
+++ src/main/java/cn/tellwhy/algorithm/isodata/ISOData.java
@@ -3,7 +3,9 @@ package cn.tellwhy.algorithm.isodata;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 * 代码未做异常处理，为了获得错误
@@ -15,6 +17,8 @@ public class ISOData {
     double theta_c = 3; //θc 两个聚类中心间的最小距离，若小于此数，两个聚类需进行合并；
     int L = 100; // 在一次迭代运算中可以合并的聚类中心的最多对数；
     int I = 10000; // 迭代运算的次数。
+
+    int alreadyRunCount = 0;
 
     List<Cluster> initClusters = new ArrayList<>();
 
@@ -33,13 +37,91 @@ public class ISOData {
 
         //此处判断迭代次数，回头再写 5|6|7
         //3
-        calculateClusterStandardDeviation(totalAverage);
+        boolean splitted = splitCluster(totalAverage);
+        if (splitted) {
+            alreadyRunCount ++;
+            // 跳2，改递归
+        }
+    }
+
+    /*
+    * 3.2 合并
+    * */
+    private void calculateCenterDistance() {
+        Map<Integer, Double> centerDistances = new HashMap<>();// index,distance
+        List<Integer> clusterIndex1 = new ArrayList<>();//保存center1
+        List<Integer> clusterIndex2 = new ArrayList<>();//保存center2
+        //小于规定距离的合并
+        //取任意两个中心的距离，小于规定距离，保存下来
+        //判断是否有重合点，如果有重合，合并其中较小的或先来的
+        int index = 0;//保存入选在列表中的索引
+        for (int i = 0; i < initClusters.size(); i++) { //n!
+            Point center1 = initClusters.get(i).getCenter();
+            for (int j = i + 1; j < initClusters.size(); j++) {
+                Point center2 = initClusters.get(i).getCenter();
+                double currentDistance = center1.distanceEuclidean(center2);
+                if (currentDistance < theta_c) {
+                    //判断节点是否已经入选，入选则根据距离大小保留小的
+                    //如果center1存在，判断center2是否存在，同时存在则三取其一
+                    //三个的情况还包括center1或2中同时有i和j
+                    //如新的较小，去掉原来的，三个集合同时修改
+                    //只有两个就二取一
+                    //不存在直接加
+                    //因为循环顺序，还需要判断j是否在clusterIndex1的情况
+                    if (!clusterIndex1.contains(i) && !clusterIndex1.contains(j) && !clusterIndex1.contains(j)) {
+                        clusterIndex1.add(index, i);
+                        clusterIndex2.add(index, j);
+                        centerDistances.put(index, currentDistance);
+                        index++;
+                    }
+                    //先判断三个的
+                    //再判断两个的
+
+                    if (clusterIndex1.contains(i)) {
+                        int keyI = clusterIndex1.indexOf(i);
+                        if (keyI > -1) {
+                            double existDistanceI = centerDistances.get(keyI);
+                            if (existDistanceI > currentDistance) {
+                                if (clusterIndex2.contains(j)) {
+                                    int keyJ = clusterIndex1.indexOf(j);
+                                    if (keyJ > -1) {
+                                        double existDistanceJ = centerDistances.get(keyJ);
+
+                                    }
+                                }
+                            } else {
+
+                            }
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //调用四次传，两次传相反参数，两次传两个List相同
+    private void checkCenterDistanceExistTwo(final List<Integer> clusterIndex1, final List<Integer> clusterIndex2,
+                                             final int i1, final int i2, final Map<Integer, Double> centerDistances) {
+        if (clusterIndex1.contains(i1) && clusterIndex2.contains(i2)) {
+            //统一列即包含i又包含j，放到三个里处理
+        }
+    }
+
+    //调用两次传相反参数
+    private void checkCenterDistanceExistOne(final List<Integer> clusterIndex1, final List<Integer> clusterIndex2,
+                                             final int i1, final int i2, final Map<Integer, Double> centerDistances) {
+        if (clusterIndex1.contains(i1) && !clusterIndex2.contains(i2)) {
+            //统一列即包含i又包含j，放到三个里处理
+        }
     }
 
     /*
     * 3.1 类分裂
     * */
-    private void calculateClusterStandardDeviation(double totalAverage) {
+    private boolean splitCluster(double totalAverage) {
+        boolean result = false;
         List<Cluster> deleteCluster = new ArrayList<>();
         for (Cluster eachCluster : initClusters) {
             Pair<Double, String> top = eachCluster.maxStandardDeviation();
@@ -53,6 +135,8 @@ public class ISOData {
                 newClusters.add(new Cluster().setCenter(newCenter2));
                 initClusterDistribution(newClusters, eachCluster.getPoints());
                 deleteCluster.add(eachCluster);
+                initClusters.addAll(newClusters);
+                result = true;
             }
         }
 
@@ -61,7 +145,8 @@ public class ISOData {
             initClusters.remove(eachCluster);
         }
         deleteCluster.clear();
-        //跳转到2
+
+        return result;
     }
 
     /*

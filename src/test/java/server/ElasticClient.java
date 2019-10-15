5371df6122b6035f76ea7b77ea353e80f8ea18b4
saaavsaaa <draculav@hotmail.com>
2019-10-15 10:04:30 +0800

    es

--- src/test/java/server/ElasticClient.java
+++ src/test/java/server/ElasticClient.java
@@ -5,9 +5,13 @@ import org.apache.http.HttpHost;
 import org.apache.http.message.BasicHeader;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -21,6 +25,7 @@ public class ElasticClient {
                 new HttpHost("sl010a-analysisdb2", 9200, "https"),
                 new HttpHost("sl010a-analysisdb3", 9200, "https")
         );
+
         Header[] defaultHeaders = new Header[]{new BasicHeader("Authorization", "Basic YWRtaW46YWRtaW4=")};
         builder.setDefaultHeaders(defaultHeaders);
         RestClient restClient = builder.build();
@@ -29,5 +34,30 @@ public class ElasticClient {
         GetRequest getRequest = new GetRequest("test-index", "test-all", "26269");
         GetResponse getResponse = client.get(getRequest);
         System.out.println(getResponse.getSourceAsString());
+    }
+
+    @Test
+    public void search() throws IOException {
+        String elasticsearchHost = "sl010a-analysisdb1:9200,sl010a-analysisdb2:9200,sl010a-analysisdb3:9200";
+        String[] elasticHosts = elasticsearchHost.split(",");
+        HttpHost[] httpHosts = new HttpHost[elasticHosts.length];
+        for(int i = 0; i < elasticHosts.length; i++) {
+            String ip = elasticHosts[i].split(":")[0];
+            String port = elasticHosts[i].split(":")[1];
+            httpHosts[i] = new HttpHost(ip, Integer.parseInt(port), "https");
+        }
+        RestClient restClient = RestClient.builder(httpHosts)
+                .setDefaultHeaders(new Header[] {new BasicHeader("Authorization", "Basic Q049ZGVtb3VzZXI6Q049ZGVtb3VzZXI=")})
+                .build();
+        RestHighLevelClient client = new RestHighLevelClient(restClient);
+
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse searchResponse = client.search(searchRequest);
+
+        System.out.println(searchResponse.getTotalShards());
     }
 }

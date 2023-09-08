10d2115ba1ef3438c6f6735865c5ce18dc122436
saaavsaaa <draculav@hotmail.com>
2023-09-08 10:14:22 +0800

    thunder

--- src/test/java/DailyTest.java
+++ src/test/java/DailyTest.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import sun.misc.BASE64Decoder;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,18 +16,45 @@ public class DailyTest {
     public void buildP(){
         int[] elec = new int[]{1, 2, 3, 4, 5};
     }
-    
+
+    @Test
+    public void aaa() throws IOException {
+        String origin = "thunder://QUFodHRwOi8vdmlweHouYm9jYWktenVpZGEuY29tLzIwMTIvRMfYuLMtMDUubXA0Wlo==";
+        origin = origin.substring(10);
+        System.out.println(origin);
+        BASE64Decoder decoder = new BASE64Decoder();
+        String content =  new String(decoder.decodeBuffer(origin),"GBK"); // "UTF-8"
+        System.out.println(content.replace("AA", "").replace("ZZ", ""));
+    }
+
+
     @Test
     public void test(){
-        ExpressionParser parser=new SpelExpressionParser();
+        ExpressionParser parser = new SpelExpressionParser();
         String expression = "T(java.lang.Runtime).getRuntime().exec('find . -name a*')";
         String result = parser.parseExpression(expression).getValue().toString();
         System.out.println(result);
     }
     
     public static void main(String[] args) {
-        DailyTest test = new DailyTest();
-        test.test(10);
+        DailyTest dailyTest = new DailyTest();
+        int length = dailyTest.buy(99999);
+        System.out.println("length:" + length);
+    }
+
+    public int buy(int n) {
+        int length = 1;
+        while (n > 1) {
+            //n = (n % 2 == 1) ? n * 3 + 1 : n / 2;
+            if (n % 2 == 1) {
+                n = n * 3 + 1;
+            } else {
+                n /= 2;
+            }
+            System.out.println("n:" + n);
+            length++;
+        }
+        return length;
     }
     
     private void test(int i){
--- src/test/java/server/ElasticClient.java
+++ src/test/java/server/ElasticClient.java
@@ -1,33 +1,33 @@
-package server;
-
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.message.BasicHeader;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.junit.Test;
-
-import java.io.IOException;
-
-public class ElasticClient {
-
-    @Test
-    public void get() throws IOException {
-        RestClientBuilder builder = RestClient.builder(
-                new HttpHost("sl010a-analysisdb1", 9200, "https"),
-                new HttpHost("sl010a-analysisdb2", 9200, "https"),
-                new HttpHost("sl010a-analysisdb3", 9200, "https")
-        );
-        Header[] defaultHeaders = new Header[]{new BasicHeader("Authorization", "Basic YWRtaW46YWRtaW4=")};
-        builder.setDefaultHeaders(defaultHeaders);
-        RestClient restClient = builder.build();
-        RestHighLevelClient client = new RestHighLevelClient(restClient);
-
-        GetRequest getRequest = new GetRequest("test-index", "test-all", "26269");
-        GetResponse getResponse = client.get(getRequest);
-        System.out.println(getResponse.getSourceAsString());
-    }
-}
+//package server;
+//
+//import org.apache.http.Header;
+//import org.apache.http.HttpHost;
+//import org.apache.http.message.BasicHeader;
+//import org.elasticsearch.action.get.GetRequest;
+//import org.elasticsearch.action.get.GetResponse;
+//import org.elasticsearch.client.RestClient;
+//import org.elasticsearch.client.RestClientBuilder;
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.junit.Test;
+//
+//import java.io.IOException;
+//
+//public class ElasticClient {
+//
+//    @Test
+//    public void get() throws IOException {
+//        RestClientBuilder builder = RestClient.builder(
+//                new HttpHost("sl010a-analysisdb1", 9200, "https"),
+//                new HttpHost("sl010a-analysisdb2", 9200, "https"),
+//                new HttpHost("sl010a-analysisdb3", 9200, "https")
+//        );
+//        Header[] defaultHeaders = new Header[]{new BasicHeader("Authorization", "Basic YWRtaW46YWRtaW4=")};
+//        builder.setDefaultHeaders(defaultHeaders);
+//        RestClient restClient = builder.build();
+//        RestHighLevelClient client = new RestHighLevelClient(restClient);
+//
+//        GetRequest getRequest = new GetRequest("test-index", "test-all", "26269");
+//        GetResponse getResponse = client.get(getRequest);
+//        System.out.println(getResponse.getSourceAsString());
+//    }
+//}
